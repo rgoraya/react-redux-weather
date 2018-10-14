c2cef5ee40fa8e67d4b3a53567c16030541f6fe0
raminder goraya <raminder.goraya@gmail.com>
2018-10-13 21:29:59 -0700

    Updates

---
+++ precache-manifest.00843c437bbeaabd2c83dbe870a29a18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/react-redux-weather/static/media/owfont-regular.f3ce0ef1.svg"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/react-redux-weather/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/react-redux-weather/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/react-redux-weather/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "714b89a36770af29209bdb75f2a92b03",
+    "url": "/react-redux-weather/static/media/city_placeholder.714b89a3.png"
+  },
+  {
+    "revision": "e1b086927702b8c803a3",
+    "url": "/react-redux-weather/static/js/runtime~main.e1b08692.js"
+  },
+  {
+    "revision": "42d5e7f873b658b3ceef",
+    "url": "/react-redux-weather/static/js/main.42d5e7f8.chunk.js"
+  },
+  {
+    "revision": "6a97e946c6873ed59180",
+    "url": "/react-redux-weather/static/js/1.6a97e946.chunk.js"
+  },
+  {
+    "revision": "42d5e7f873b658b3ceef",
+    "url": "/react-redux-weather/static/css/main.a8e8850a.chunk.css"
+  },
+  {
+    "revision": "96f5e471b2ddc490683030eb00579cd3",
+    "url": "/react-redux-weather/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import weatherForCity from './weather_for_city'
-
-const rootReducer = combineReducers({
-  weatherForCity: weatherForCity
-})
-
-export default rootReducer
